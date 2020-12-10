27305f960fb7b785333e52461ca25c9d9171dc70
Artem  Baranovskyi <artem.baranovsky1980@gmail.com>
2020-12-10 16:37:23 +0200

    10 Grid Spacing and Card Tweaks

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,20 +1,26 @@
 @extends ('layouts.app')
 @section('content')
-
-    <div class="flex items-center mb-3">
+    <header class="flex items-center mb-3 py-4">
 {{--        <h1 class="mr-auto">Birdboard</h1>--}}
-        <a href="/projects/create">New Project</a>
-    </div>
+        <div class="flex justify-between items-center w-full">
+                <h2 class="text-gray text-2xl text-sm font-normal">My Projects</h2>
+            <a class="button" href="/projects/create">New Project</a>
+        </div>
+    </header>
 
-    <div class="flex">
+    <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse($projects as $project)
-            <div class="bg-white mr-3 p-5 rounded shadow w-1/3" style="height: 200px;">
-                <h3 class="font-semibold font-normal text-xl py-4">{{ $project->title }}</h3>
-{{--                <div>{{ Illuminate\Support\Str::limit($project->description) }}</div>--}}
-                <div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+            <div class="lg:w-1/3 px-3 pb-6">
+                <div class="bg-white p-5 rounded-lg shadow" style="height: 200px;">
+                    <h3 class="font-semibold font-normal text-xl py-4 mb-3 -ml-5 border-l-4 border-blue-light pl-4">
+                        <a href="{{ $project->path() }}" class="text-back no-underline">{{ $project->title }}</a>
+                    </h3>
+    {{--                <div>{{ Illuminate\Support\Str::limit($project->description) }}</div>--}}
+                    <div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+                </div>
             </div>
         @empty
             <div>No projects yet.</div>
         @endforelse
-    </div>
+    </main>
 @endsection
--- tailwind.js
+++ tailwind.js
@@ -3,9 +3,12 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
-      // 'grey' : '#b8c2cc',
-      'grey' : 'rgba(0,0,0,0.4)',
-      'grey-light' : '#F5F6F9',
+        // 'grey' : '#b8c2cc',
+        'grey' : 'rgba(0,0,0,0.4)',
+        'grey-light' : '#F5F6F9',
+        'blue': '#47cdff',
+        'blue-light': '#8ae2fe',
+        'white': '#ffffff',
     },
     extend: {
         shadows: {
